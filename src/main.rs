a34f0129eb8356ac2a3f7bfbfbb8bdd01f06fcab
DeviousCilantro <archisman@mailbox.org>
2023-06-20 21:16:36 +0530

    Implement Unix socket API support

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,13 @@ use argon2::{
 };
 use sha3::{Digest, Keccak512};
 use generic_array::GenericArray;
-use std::io::{self, Write};
+use std::io::{
+    Write, BufRead, BufReader
+};
 use std::collections::HashMap;
 use std::error::Error;
+use std::os::unix::net::{UnixStream, UnixListener};
+use std::fs::{metadata, remove_file};
 
 #[derive(Debug, Eq, Hash, PartialEq, Clone)]
 struct LoginData {
@@ -19,9 +23,8 @@ struct LoginData {
     encoded_nonce: String,
 }
 
-
 impl LoginData {
-    fn add_login(credentials: &str) -> Result<Self, Box<dyn Error>> {
+    fn add_login(credentials: &str, stream: &UnixStream) -> Result<Self, Box<dyn Error>> {
         let credentials: Vec<&str> = credentials.split('&').collect();
         let mut key = [0u8; 32];
         if let Err(e) = Argon2::default().hash_password_into(
@@ -32,11 +35,16 @@ impl LoginData {
             panic!("{e}");
         };
 
-        let identifier = read_input("Enter identifier: ")?;
-        let username = read_input("Enter username: ")?;
-        let password = rpassword::prompt_password("Enter password: ")?;
+        write(stream, "\nEnter identifier: ");
+        let identifier = read(stream)?;
+        write(stream, "Enter username: ");
+        let username = read(stream)?;
+        write(stream, "Enter password on the server-side terminal. Waiting...\n");
+        let password = rpassword::prompt_password("\nEnter password: ")?;
+        write(stream, "\nConfirm password on the server-side terminal. Waiting...\n");
         let confirm_password = rpassword::prompt_password("Confirm password: ")?;
         assert_eq!(password, confirm_password, "Passwords do not match");
+        println!("\nPasswords match.");
         let mut login_data = format!("{username}:{password}");
         let key = GenericArray::from_slice(&key);
         let cipher = XChaCha20Poly1305::new(key);
@@ -86,36 +94,63 @@ impl LoginData {
     }
 }
 
-fn read_input(prompt: &str) -> Result<String, Box<dyn Error>> {
-    let mut input = String::new();
-    print!("{prompt}");
-    io::stdout().flush()?;
-    io::stdin().read_line(&mut input)?;
-    Ok(input.clone().trim().to_string())
-}
-
-fn hashed_credentials(register: bool) -> Result<String, Box<dyn Error>> {
+fn hashed_credentials(register: bool, stream: &UnixStream) -> Result<String, Box<dyn Error>> {
     let mut hasher = Keccak512::new();
-    let email = read_input("Enter username or email ID: ")?;
-    let password = rpassword::prompt_password("Enter master password: ")?;
+    write(stream, "Enter username or email ID: ");
+    let email = read(stream)?;
+    write(stream, "Enter master password on the server-side terminal. Waiting...\n");
+    let password = rpassword::prompt_password("\nEnter password: ")?;
     hasher.update(format!("{email}:{password}"));
     if register {
-        let confirm_password = rpassword::prompt_password("Confirm master password: ")?;
+        write(stream, "\nConfirm master password on the server-side terminal. Waiting...\n");
+        let confirm_password = rpassword::prompt_password("Confirm password: ")?;
         assert_eq!(password, confirm_password, "Passwords do not match");
+        println!("\nPasswords match.");
     }
     Ok(format!("{:x}", hasher.finalize()))
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn read(stream: &UnixStream) -> Result<String, Box<dyn Error>> {
+    let mut reader = BufReader::new(stream);
+    let mut response = String::new();
+    reader.read_line(&mut response)?;
+    let response = response.trim().to_string();
+    Ok(response)
 
+}
+
+
+fn write(stream: &UnixStream, text: &str) {
+    let Ok(mut unix_stream) = stream.try_clone() else { 
+        panic!("\nCannot clone stream");
+    };
+
+    if let Err(e) = unix_stream.write(text.as_bytes()) {
+        panic!("{e}");
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let socket_path = "/tmp/password-manager.sock";
+    if metadata(socket_path).is_ok() {
+        println!("\nA socket is already present at {}. Deleting...", &socket_path);
+        remove_file(socket_path)?;
+    }
+    let listener = UnixListener::bind(socket_path)?;
+    println!("\nCreating UNIX socket at {socket_path}...");
+    println!("\nCommunicate using a utility like netcat. For instance, execute `nc -U /tmp/password-manager.sock` on a separate terminal.");
+    let stream = listener.accept()?.0;
     let mut database: HashMap<String, Vec<LoginData>> = HashMap::new();
 
-    loop {
-        let choice = read_input("\n[R]egister / [A]uthenticate / [E]xit? (r/a/e) ")?;
 
+    loop {
+
+    write(&stream, "\n[R]egister / [A]uthenticate / [E]xit? (r/a/e) ");
+
+        let choice = read(&stream)?;
         match choice.as_str() {
             "R" | "r" => {
-                let authenticator = hashed_credentials(true)?;
+                let authenticator = hashed_credentials(true, &stream)?;
                 if let Some((_, _)) = database
                     .clone()
                         .iter()
@@ -129,24 +164,26 @@ fn main() -> Result<(), Box<dyn Error>> {
                 database.insert(credentials, Vec::new());
             },
             "A" | "a" => {
-                let authenticator = hashed_credentials(false)?;
+                let authenticator = hashed_credentials(false, &stream)?;
                 if let Some((key, _)) = database
                     .clone()
                         .iter()
                         .find(|(key, _)| key.contains(&authenticator))
                         {
-                            println!("\nAuthentication sucessful.");
+                            write(&stream, "\nAuthentication sucessful.\n");
                             loop {
-                                let choice = read_input("\n[A]dd entry / [R]etrieve entry / [L]ogout? (a/r/l) ")?;
+                                write(&stream, "\n[A]dd entry / [R]etrieve entry / [L]ogout? (a/r/l) ");
+                                let choice = read(&stream)?;
                                 match choice.as_str() {
                                     "A" | "a" => {
                                         if let Some(vector) = database.get_mut(key) {
-                                            vector.push(LoginData::add_login(key)?);
-                                            println!("Added entry to database: {vector:?}");
+                                            vector.push(LoginData::add_login(key, &stream)?);
+                                            write(&stream, &format!("Added entry to database: {vector:?}"));
                                         }
                                     },
                                     "R" | "r" => {
-                                        let identifier = read_input("\nEnter identifier: ")?;
+                                        write(&stream, "\nEnter identifier: ");
+                                        let identifier = read(&stream)?;
                                         if let Some(vector) = database.get(key) {
                                             if let Some(login_data) = vector
                                                 .clone()
@@ -155,23 +192,27 @@ fn main() -> Result<(), Box<dyn Error>> {
                                                     {
                                                         let credentials = login_data.retrieve_login(key)?;
                                                         let credentials: Vec<&str> = credentials.split(':').collect();
-                                                        println!("\nUsername: {}", credentials[0]);
-                                                        println!("Password: {}", credentials[1]);
+                                                        write(&stream, &format!("\nUsername: {}", credentials[0]));
+                                                        write(&stream, &format!("\nPassword: {}\n", credentials[1]));
                                                     } else {
-                                                        eprintln!("\nInvalid identifier.");
+                                                        write(&stream, "\nInvalid identifier.\n");
                                                     }
                                         }
                                     },
                                     "L" | "l" => break,
-                                    _ => eprintln!("\nInvalid input."),
+                                    _ => { 
+                                        write(&stream, "\nInvalid input.\n");
+                                    }
                                 };
                             };
                         } else {
-                            eprintln!("\nInvalid credentials.");
+                            write(&stream, "\nInvalid credentials.\n");
                         }
             },
             "E" | "e" => break,
-            _ => eprintln!("\nInvalid input"),
+            _ => {
+                write(&stream, "\nInvalid input\n");
+            },
         }
     }
 
